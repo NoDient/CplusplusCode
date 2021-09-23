ae8fc8f7a060dbc1a425a80cf0be5ca86476690f
NooDient <91038648+NoDient@users.noreply.github.com>
2021-09-23 21:20:52 +0700

    Update StandardizeString.cpp

--- StandardizeString.cpp
+++ StandardizeString.cpp
@@ -2,18 +2,23 @@
 #include <cstring>
 using namespace std;
 
+// Hàm xóa các khoảng trắng đầu chuỗi
 void BegStandardize(char *&str)
 {
 	unsigned int i = 0;
 	while (str[i++] == ' ');
 	str = str + i - 1;
 }
+
+// Hàm xóa khoảng trắng ở cuối chuỗi
 void EndStandardize(char *str)
 {
 	unsigned int i = strlen(str) - 1;
 	while (str[i--] == ' ');
 	str[i + 2] = '\0';
 }
+
+// Hàm xóa khoảng trắng thừa trong nội dung chuỗi
 void CurStandardize(char *str)
 {
 	unsigned int diff = 0;
