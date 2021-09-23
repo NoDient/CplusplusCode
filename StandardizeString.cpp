12b8c85cd31d87ff8c86aa5d4f7b5911bc409bf1
NooDient <91038648+NoDient@users.noreply.github.com>
2021-09-23 21:18:19 +0700

    Add files via upload

---
+++ StandardizeString.cpp
@@ -0,0 +1,43 @@
+#include <iostream>
+#include <cstring>
+using namespace std;
+
+void BegStandardize(char *&str)
+{
+	unsigned int i = 0;
+	while (str[i++] == ' ');
+	str = str + i - 1;
+}
+void EndStandardize(char *str)
+{
+	unsigned int i = strlen(str) - 1;
+	while (str[i--] == ' ');
+	str[i + 2] = '\0';
+}
+void CurStandardize(char *str)
+{
+	unsigned int diff = 0;
+	unsigned len = strlen(str) - 1;
+	for (unsigned int i = 1; i < len; i++)
+	{
+		while (str[i] == ' ' && str[i+1] == ' ')
+		{
+			for (unsigned int j = i; j < len-1; j++)
+			{
+				str[j] = str[j+1];
+			}
+			str[len--] = '\0';
+		}
+	}
+}
+
+int main(){
+	char *s = new char[100];
+	cin.getline(s, 100);
+	BegStandardize(s);
+	CurStandardize(s);
+	EndStandardize(s);
+	cout << s;
+	return 0;
+}
+
