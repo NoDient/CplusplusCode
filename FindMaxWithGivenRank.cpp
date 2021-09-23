d6db0f9449b96c92877e5221c430b5039d615187
NooDient <91038648+NoDient@users.noreply.github.com>
2021-09-23 18:41:59 +0700

    Add files via upload

---
+++ FindMaxWithGivenRank.cpp
@@ -0,0 +1,24 @@
+#include <iostream>
+using namespace std;
+
+int FindMaxWithGivenRank(int *arr, int size, int rank = 1)
+{
+	int count = 0;
+	for (int i = 0; i < size; i++)
+	{
+		for (int j = 0; j < size; j++)
+		{
+			if (arr[j] > arr[i]) count++;
+		}
+		if (rank-1 == count) return arr[i];
+		else count = 0; // reset count
+	}
+}
+
+int main()
+{
+	int arr[10] = {1, 2, 3, 4, 5, 6, 7, 8 , 9, 10};
+	int a = FindMaxWithGivenRank(arr, 10, 6);
+	cout << a << endl;
+	return 0;
+}
